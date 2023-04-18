289cc10adc73577f38306fd7ae1bbda2e532ae1a
EmranEmon47 <aa.emran47@gmail.com>
2023-04-19 00:10:17 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Shared/NavBar/NavBar'
+import Hero from './Hero/Hero'
+import Footer from '../Shared/Footer/Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <NavBar/>
+        {/* <Hero/>
+        <Footer/> */}
+    </div>
+  )
+}
